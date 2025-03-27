df72bed5f195d78fa34fa4542f78ebffc00bc731
Gandhi Rajan M <gandhirajan23456@gmail.com>
2025-03-27 16:01:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Additional JavaScript for interactivity can be added here
